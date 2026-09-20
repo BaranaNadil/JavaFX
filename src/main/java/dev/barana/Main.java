29b8e8ab66eb28a1a87bfb7d42544ad8db13f18d
BaranaNadil <161505829+BaranaNadil@users.noreply.github.com>
2026-09-20 23:02:38 +0530

    Initial commit

---
+++ src/main/java/dev/barana/Main.java
@@ -0,0 +1,25 @@
+package dev.barana;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main extends Application {
+
+
+    static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stage.setTitle("Hello JavaFX");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
---
+++ src/main/java/dev/barana/MainController.java
@@ -0,0 +1,4 @@
+package dev.barana;
+
+public class MainController {
+}
